615c59afb7d825278ffe30dfd6a026ab3036175d
Chris Gerber <ibmcloudcourse76@gmail.com>
2023-03-25 01:54:17 -0400

    Adding temporary changes to Github

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -4,21 +4,41 @@ from django.utils.timezone import now
 
 # Create your models here.
 
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = models.CharField(null=False, max_length=30, default='Make')
+    description = models.CharField(max_length=200, default='Description')
+    
+    def __str__(self):
+        return "Name: " + self.name + ", " + \
+               "Description: " + self.description
 
+class CarModel(models.Model):
+    make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    id = models.IntegerField(default=1,primary_key=True)
+    name = models.CharField(null=False, max_length=30, default='Car')
 
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
-# - Name
-# - Dealer id, used to refer a dealer created in cloudant database
-# - Type (CharField with a choices argument to provide limited choices such as Sedan, SUV, WAGON, etc.)
-# - Year (DateField)
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
+    SEDAN = 'Sedan'
+    SUV = 'SUV'
+    WAGON = 'Wagon'
+    CAR_TYPES = [
+        (SEDAN, 'Sedan'),
+        (SUV, 'SUV'),
+        (WAGON, 'Wagon')
+    ]
+    type = models.CharField(
+        null=False,
+        max_length=50,
+        choices=CAR_TYPES,
+        default=SEDAN
+    )
+
+    year = models.DateField(default=now)
+
+    def __str__(self):
+        return "Dealer ID: " + self.id + ", " + \
+               "Name: " + self.name + ", " + \
+               "Type: " + self.type + ", " + \
+               "Year: " + str(self.year)
 
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,7 +1,11 @@
 import requests
 import json
-# import related models here
+#from .models import CarDealer, DealerReview
 from requests.auth import HTTPBasicAuth
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from ibm_watson import NaturalLanguageUnderstandingV1
+from ibm_watson.natural_language_understanding_v1 import Features,SentimentOptions
+import time
 
 
 # Create a `get_request` to make HTTP GET requests
